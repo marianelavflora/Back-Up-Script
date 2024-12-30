acbd4ccc4275ab9609eba77d89ece809c062a03f
Marianela Victoria Flora <70304446+marianelavflora@users.noreply.github.com>
2024-12-30 03:44:08 -0300

    Update backup.bat

--- backup.bat
+++ backup.bat
@@ -1,11 +1,11 @@
 @echo off
 
 rem LOCAL COPY TO HDD
-xcopy /s /e /k /c /y /d /i /h /r x:\FolderToBackup x:\BackupFolderDestinationHDD
+xcopy /s /e /k /c /y /d /i /h /r x:\SourceFolder x:\BackupFolderHDD
 echo Backup to HDD completed! 
 
 rem LOCAL COPY TO USB DEVICE
-xcopy /s /e /k /c /y /d /i /h /r x:\FolderToBackup x:\BackupFolderDestinationUSB
+xcopy /s /e /k /c /y /d /i /h /r x:\SourceFolder x:\BackupFolderUSB
 echo Backup to USD Devide completed!  
 
 @pause
